98b0139b04cab9245b093bf33e6344f56ffb954a
DavideGiuseppeCiliento <cilientodg@gmail.com>
2025-06-16 17:21:26 +0200

    Aggiunto sia l'INDEX ce lo SHOW nel controller.

--- controllers/movies_controller.js
+++ controllers/movies_controller.js
@@ -8,9 +8,18 @@ const urlImage = "http://localhost:3000/images/";
 // # FUNCTION
 //--- IMAGE URL FUNCTION
 function setImageUrl(data) {
-  data.map((movie) => {
-    return (movie.image = `${urlImage}${movie.image}`);
-  });
+  // Se data è un ARRAY
+  if (Array.isArray(data)) {
+    data.forEach((movie) => {
+      movie.image = movie.image ? `${urlImage}${movie.image}` : null;
+    });
+    // Se data esiste ed è un OGGETTO
+  } else if (data && typeof data === "object") {
+    data.image = data.image ? `${urlImage}${data.image}` : null;
+    // Se non esiste o non è valido
+  } else {
+    console.warn("setImageUrl ha ricevuto un valore non valido:", data);
+  }
 }
 
 // # CONTROLLERS
@@ -28,5 +37,46 @@ function index(req, res) {
   });
 }
 
+//--- SHOW FUNCTION
+
+function show(req, res) {
+  let movie = {};
+  const { id } = req.params;
+
+  const sqlMovieId = `SELECT * 
+                    FROM movies
+                    WHERE movies.id = ?;  `;
+
+  const sqlMovieIdReview = `SELECT * 
+                    FROM reviews
+                    WHERE movie_id = ?;  `;
+
+  connection.query(sqlMovieId, [id], (err, results) => {
+    if (err) return res.status(500).json({ error: "Database query failed" });
+
+    if (results.length === 0) {
+      return res.status(404).json({ message: "Film non trovato" });
+    }
+    // Il singolo film
+    movie = results[0];
+    setImageUrl(movie);
+
+    connection.query(sqlMovieIdReview, [id], (err, reviewResults) => {
+      if (err) return res.status(500).json({ error: "Database query failed" });
+
+      const reviewsMovie = reviewResults.map((review) => ({
+        id: review.id,
+        name: review.name,
+        vote: review.vote,
+        text: review.text,
+      }));
+
+      movie = { ...movie, reviews: reviewsMovie };
+
+      res.json(movie);
+    });
+  });
+}
+
 // # EXPORT
-export default { index };
+export default { index, show };
--- routes/movies_routes.js
+++ routes/movies_routes.js
@@ -9,5 +9,8 @@ const router = express.Router();
 //--- INDEX
 router.get(`/`, movies_controller.index);
 
+//--- SHOW
+router.get(`/:id`, movies_controller.show);
+
 // # EXPORT
 export default router;
