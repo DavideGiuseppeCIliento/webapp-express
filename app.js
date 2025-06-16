8872f5e277ea4a9b7ceb4d28b02f8f5817bc3c30
DavideGiuseppeCiliento <cilientodg@gmail.com>
2025-06-16 17:28:46 +0200

    Ho creato la gestione degli errori .

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
 // # IMPORT
-//--- IMPORT ROUTER
-import movies_routes from "./routes/movies_routes.js";
 //--- IMPORT EXPRESS
 import express from "express";
+
+//--- IMPORT ROUTER
+import movies_routes from "./routes/movies_routes.js";
+
+// --- IMPORT MIDDLEWARE
+import errorHandler from "./middlewares/errorhandler.js";
+import notFound from "./middlewares/notFound.js";
 
 // # GLOBAL DATA
 const app = express();
 const port = 3000;
 
-// # MIDDLEWARE
+// # SET PUBLIC DIRECTORY
 app.use(express.static("public"));
 
 // # CALL ROUTES
 app.use("/movies", movies_routes);
 
-app.listen(port, () => {
-  console.log(`✅ Server in ascolto su http://localhost:${port}`);
+// # MIDDLEWARE ERROR
+app.use(errorHandler);
+
+// # MIDDLEWARE NOT FOUND
+app.use(notFound);
+
+app.listen(port, (error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(`Lettura della porta: ${port}`);
+  }
 });
